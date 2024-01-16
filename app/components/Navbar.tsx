3c2db960ce3e45fcb44ea6a5492a719c1baa3487
rominadouk <douk.romina@gmail.com>
2024-01-15 20:56:37 -0800

    added basic project layout and basic hamburger menu layout on mobile view

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,40 @@
+'use client'
 import React from 'react'
+import { useState } from 'react'
 import Image from 'next/image'
 import hamburgerMenu from '../../public/assets/icons/hamburger-menu.svg'
+import Link from 'next/link'
 
 const Navbar = () => {
+    const [hamburgerMenuIsOpen, setHamburgerMenuIsOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setHamburgerMenuIsOpen(!hamburgerMenuIsOpen)
+    };
+    console.log(hamburgerMenuIsOpen)
+    
   return (
-    <div className='flex justify-between mx-4 mt-4'>
+    <nav className={ hamburgerMenuIsOpen ? ' z-50 pt-4 w-full px-5 absolute bg-white' : 'z-50 pt-4 w-full px-5 absolute'}>
+        <div className='flex justify-between'>
         <div className='flex gap-3'>
             <p>RD</p>
             <p>Romina Douk</p>
         </div>
-        <Image src={hamburgerMenu}  alt='three-line-menu' />
-    </div>
+        <Image src={hamburgerMenu}   className='lg:hidden' alt='three-line-menu' onClick = {toggleMobileMenu}/>
+        </div>
+        {
+            hamburgerMenuIsOpen && (
+                <div className='flex flex-col py-5 relative bg-white gap-4 text-center'>
+                    <p>Home</p>
+                    <p>About</p>
+                    <p>Contact</p>
+                    <p>LinkedIn</p>
+                    <p>Github</p>
+                    
+                </div>
+            )
+        }
+    </nav>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image';
 import './page.css'
 import Navbar from './components/Navbar';
+import zenImg from '../public/assets/images/zen-home.png'
+import jserviceImg from '../public/assets/images/j-service.png'
+import welspotLandingImg from '../public/assets/images/community-landing.png'
 
 
 
@@ -9,17 +12,17 @@ export default function Home() {
      const projects = [
       {
       title: 'zen',
-      image: 'import image and add url here',
+      image: zenImg,
       link: 'insert the link here'
       }, 
       {
       title: 'jservice',
-      image: 'import image and add url here',
+      image: jserviceImg,
       link: 'insert the link here'
       }, 
       {
-      title: 'yanigaiba',
-      image: 'import image and add url here',
+      title: 'welspot landing',
+      image: welspotLandingImg,
       link: 'insert the link here'
       }, 
     
@@ -37,19 +40,20 @@ export default function Home() {
         <p className='hi'>Hi, I'm</p>
         <div id='home-portrait' className='h-52 rounded-lg w-80 relative self-center'> 
         </div>
-        <h1 className='text-5xl mt-3'>Romina Douk</h1>
-        <p>Full-Stack Software Developer</p>
+        <h1 className='text-5xl mt-3 font-medium'>Romina Douk</h1>
+        <p className='font-medium'>Full-Stack Software Developer</p>
       </div>
     <section id='projects' className='flex flex-col items-center mt-10 gap-5'>
       {projects.map((project) => {
         return (
-          <div key={project.title} className='flex flex-col h-56 w-56 text-center'>
+          <div key={project.title} className='flex flex-col justify-between h-56 w-56 text-center relative'>
+            <div className=' flex-1 relative'>
+              <Image src={project.image}  className='project-image ' alt={project.title} layout='fill' objectFit='cover' />
+            </div>
             <h2 className='py-2'>{project.title}</h2>
           </div>
         )
-
       })}
-
     </section>
     </main>
   )
